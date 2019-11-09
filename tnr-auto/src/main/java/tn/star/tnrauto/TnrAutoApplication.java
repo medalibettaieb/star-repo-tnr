40c911018d9f82f4555c541be1080b3328f7fa28
Your Name <you@example.com>
2019-11-09 19:02:28 +0100

    first cmommit

---
+++ tnr-auto/src/main/java/tn/star/tnrauto/TnrAutoApplication.java
@@ -0,0 +1,13 @@
+package tn.star.tnrauto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TnrAutoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TnrAutoApplication.class, args);
+	}
+
+}
---
+++ tnr-auto/src/test/java/tn/star/tnrauto/TnrAutoApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.star.tnrauto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TnrAutoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
